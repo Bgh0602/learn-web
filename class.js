325b58359cd874fdeb231879c8f496746ae7a8a4
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-07-01 20:55:18 +0900

    learning method js

--- class.js
+++ class.js
@@ -5,15 +5,19 @@ class person {
         this.second = second;
         this.third = third;
     }
+    sum() {
+        return this.first + this.second + this.third;
+    }
 }
 
 let bae = new person('bae', 10, 10, 10);
+let hwang = new person('hwang', 20, 20, 20);
+
 console.log(bae)
 
-// bae.sum = function () {
-//     return this.first - this.second + this.third;
-// }
-// let hwang = new person('hwang', 10, 10, 10);
+bae.sum = function () {
+    return this.first - this.second + this.third;
+}
 
-// console.log(bae.sum());
-// console.log(hwang.sum());
+console.log(bae.sum());
+console.log(hwang.sum());
