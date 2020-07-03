ddab47c4aa8d970b252aeb805f12df84dcd1f2f7
Bgh0602 <57307366+Bgh0602@users.noreply.github.com>
2020-07-03 22:35:07 +0900

    making vending machine's frame

--- bigger.js
+++
@@ -1,9 +0,0 @@
-let height = 10;
-let width = 10;
-function pressButton() {
-    let bigger = document.getElementById('snowball');
-    bigger.style.height = height + 10 + 'px';
-    bigger.style.width = width + 10 + 'px';
-    height += 10;
-    width += 10;
-}
